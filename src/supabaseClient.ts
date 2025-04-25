7a798e036ecf37860b1c5233c132d4549fda03a3
Andres-Yaqueno <71290027+Andres-Yaqueno@users.noreply.github.com>
2025-04-24 20:03:04 -0500

    Integracion con supabase

--- src/supabaseClient.ts
+++ src/supabaseClient.ts
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
-const SUPABASE_URL = "https://xjfvkodpzlugwrpehkbw.supabase.co";
-const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhqZnZrb2Rwemx1Z3dycGVoa2J3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDM1NDQ0NDEsImV4cCI6MjA1OTEyMDQ0MX0.sbes16-1JEPvGNJ0QABKNjrPTdzaWXIf-0kWUHWShgg";
+const SUPABASE_URL = "https://jowwaquzicuwjzldscay.supabase.co";
+const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Impvd3dhcXV6aWN1d2p6bGRzY2F5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDM1NjE4OTYsImV4cCI6MjA1OTEzNzg5Nn0.cYI48kM9dOH8P1wt3aD1779GbWqjOsAPb0CEvPTqrPI";
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
